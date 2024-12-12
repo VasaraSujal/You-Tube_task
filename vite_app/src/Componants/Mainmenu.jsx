b77cbbfddc0af158944d1c86a6ba0ec8ffec3389
Sujal Vasara <vasarasujal.cg@gmail.com>
2024-12-12 13:28:19 +0530

    Added

--- vite_app/src/Componants/Mainmenu.jsx
+++ vite_app/src/Componants/Mainmenu.jsx
@@ -1,4 +1,143 @@
 
+// import React, { useState, useEffect } from "react";
+// import "./Mainmenu.css";
+// import img18 from "../Img/search.png";
+// import img19 from "../Img/mic.png";
+// import img20 from "../Img/create.png";
+// import img21 from "../Img/bell.png";
+// import img22 from "../Img/more.png";
+// import img23 from "../Img/Coke.png";
+
+// const API_KEY = "AIzaSyB-Zfrq-g9mxd5nnn1yw-ryXWMc9CUP9k8";
+
+// function Mainmenu() {
+//   const [videos, setVideos] = useState([]);
+//   const [query, setQuery] = useState("");
+//   const [maxResults, setMaxResults] = useState(20);
+//   const [selectedVideoId, setSelectedVideoId] = useState(null); // State for selected video ID
+
+
+// useEffect(()=>{
+//   fetchVideos();
+// }, [1]);
+
+//   // Fetch videos based on the query
+//   const fetchVideos = (searchQuery) => {
+//     const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=video&maxResults=${maxResults}&q=${searchQuery}`;
+//     fetch(API_URL)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setVideos(data.items);
+//         console.log(data.items);
+//       })
+//       .catch((error) => console.error("Error fetching YouTube data:", error));
+//   };
+
+//   const handleSearch = () => {
+//     const searchInput = document.querySelector(".search").value;
+//     if (searchInput) {
+//       setQuery(searchInput); // Set query state for potential debugging or reference
+//       fetchVideos(searchInput); // Fetch videos directly when the user searches
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="container3">
+//         <input className="search" type="text" placeholder="Search" />
+//         <div className="search1" onClick={handleSearch}>
+//           <img src={img18} alt="Search Icon" />
+//         </div>
+//         <div className="mic">
+//           <img src={img19} alt="Mic Icon" />
+//         </div>
+//         <div className="create">
+//           <img src={img20} alt="Create Icon" />
+//         </div>
+//         <div className="bell">
+//           <img src={img21} alt="Bell Icon" />
+//         </div>
+//         <div className="more">
+//           <img src={img22} alt="More Icon" />
+//         </div>
+//         <div className="profile">
+//           <img
+//             src="https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%201%20(2).png"
+//             alt="Profile"
+//           />
+//         </div>
+
+//         <hr className="new" />
+
+//         <div className="container4">
+//           <div className="child new3">All</div>
+//           <div className="child">Coke Studio</div>
+//           <div className="child">UX</div>
+//           <div className="child">Case Study</div>
+//           <div className="child">Music</div>
+//           <div className="child">Bangla Lofi</div>
+//           <div className="child">Tour</div>
+//           <div className="child">Saint Martin</div>
+//           <div className="child">Tech</div>
+//           <div className="child">iPhone 13</div>
+//           <div className="child">User Interface Design</div>
+//           <div className="child">Computer</div>
+//         </div>
+
+//         <hr className="new2" />
+
+//         <div className="container2">
+//           {selectedVideoId ? ( // Show iframe if a video is selected
+//             <div className="videoPlayer">
+//               <iframe
+//                 width="1160px"
+//                 height="500px"
+//                 src={`https://www.youtube.com/embed/${selectedVideoId}`}
+//                 frameBorder="0"
+//                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//                 allowFullScreen
+//                 title="YouTube Video Player"
+//               ></iframe>
+//               <button onClick={() => setSelectedVideoId(null)}>Close</button>
+//             </div>
+//           ) : (
+//             videos.map((video) => (
+//               <div
+//                 className="items"
+//                 key={video.id.videoId}
+//                 onClick={() => setSelectedVideoId(video.id.videoId)} // Set video ID on click
+//               >
+//                 <img
+//                   className="img2"
+//                   src={video.snippet.thumbnails.medium.url}
+//                   alt={video.snippet.title}
+//                 />
+//                 <img className="img3" src={img23} alt="Channel Icon" />
+//                 <p className="name">{video.snippet.title}</p>
+//                 <span className="name2">{video.snippet.channelTitle}</span>
+//                 <img
+//                   src="https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/verified.png"
+//                   alt="Verified"
+//                   className="img4"
+//                 /><br />
+//                 <span className="name2">1M views . 1 day ago</span>
+//               </div>
+//             ))
+//           )}
+//         </div>
+
+          
+
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Mainmenu;
+
+
+
+
 import React, { useState, useEffect } from "react";
 import "./Mainmenu.css";
 import img18 from "../Img/search.png";
@@ -7,43 +146,51 @@ import img20 from "../Img/create.png";
 import img21 from "../Img/bell.png";
 import img22 from "../Img/more.png";
 import img23 from "../Img/Coke.png";
+import Sidebar from "./Sidebar";
 
-const API_KEY = "AIzaSyBubDBOcL6ZjX6ClWh_33qWXpIg8wHsKAU";
+const API_KEY = "AIzaSyDnLYPWrbVSM-iGRH03mB7w99hg4bI_qRw";
 
 function Mainmenu() {
   const [videos, setVideos] = useState([]);
-  const [query, setQuery] = useState("music");
+  const [query, setQuery] = useState("");
+  const [maxResults, setMaxResults] = useState(2);
+  const [selectedVideoId, setSelectedVideoId] = useState(null); // State for selected video ID
+
+
+useEffect(()=>{
+  fetchVideos();
+}, [1]);
 
   // Fetch videos based on the query
   const fetchVideos = (searchQuery) => {
-    const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=video&maxResults=12&q=${searchQuery}`;
+    const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=video&maxResults=${maxResults}&q=${searchQuery}`;
     fetch(API_URL)
       .then((response) => response.json())
       .then((data) => {
         setVideos(data.items);
+        console.log(data.items);
       })
       .catch((error) => console.error("Error fetching YouTube data:", error));
   };
 
-  useEffect(() => {
-    fetchVideos(query);
-  }, [query]);
-
   const handleSearch = () => {
     const searchInput = document.querySelector(".search").value;
     if (searchInput) {
-      setQuery(searchInput);
+      setQuery(searchInput); // Set query state for potential debugging or reference
+      fetchVideos(searchInput); // Fetch videos directly when the user searches
     }
   };
 
   return (
     <>
-      <div className="container3">
-        <input
-          className="search"
-          type="text"
-          placeholder="Search"
+      <div>
+
+        <Sidebar 
+          getDataFile={fetchVideos}
         />
+
+      <div className="container3">
+        <input className="search" type="text" placeholder="Search" />
         <div className="search1" onClick={handleSearch}>
           <img src={img18} alt="Search Icon" />
         </div>
@@ -52,6 +199,7 @@ function Mainmenu() {
         </div>
         <div className="create">
           <img src={img20} alt="Create Icon" />
+          55
         </div>
         <div className="bell">
           <img src={img21} alt="Bell Icon" />
@@ -86,25 +234,49 @@ function Mainmenu() {
         <hr className="new2" />
 
         <div className="container2">
-          {videos.map((video) => (
-            <div className="items" key={video.id.videoId}>
-              <img
-                className="img2"
-                src={video.snippet.thumbnails.medium.url}
-                alt={video.snippet.title}
-              />
-              <img className="img3" src={img23} alt="Channel Icon" />
-              <p className="name">{video.snippet.title}</p>
-              <span className="name2">{video.snippet.channelTitle}</span>
-              <img
-                src="https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/verified.png"
-                alt="Verified"
-                className="img4"
-              />
-              <span className="name2">1M views . 1 day ago</span>
+          {selectedVideoId ? ( // Show iframe if a video is selected
+            <div className="videoPlayer">
+              <iframe
+                width="1160px"
+                height="500px"
+                src={`https://www.youtube.com/embed/${selectedVideoId}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="YouTube Video Player"
+              ></iframe>
+              <button onClick={() => setSelectedVideoId(null)}>Close</button>
             </div>
-          ))}
+          ) : (
+            videos.map((video) => (
+              <div
+                className="items"
+                key={video.id.videoId}
+                onClick={() => setSelectedVideoId(video.id.videoId)} // Set video ID on click
+              >
+                <img
+                  className="img2"
+                  src={video.snippet.thumbnails.medium.url}
+                  alt={video.snippet.title}
+                />
+                <img className="img3" src={img23} alt="Channel Icon" />
+                <p className="name">{video.snippet.title}</p>
+                <span className="name2">{video.snippet.channelTitle}</span>
+                <img
+                  src="https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/verified.png"
+                  alt="Verified"
+                  className="img4"
+                /><br />
+                <span className="name2">1M views . 1 day ago</span>
+              </div>
+            ))
+          )}
         </div>
+
+          
+
+      </div>
+
       </div>
     </>
   );
--- vite_app/src/Componants/Sidebar.jsx
+++ vite_app/src/Componants/Sidebar.jsx
@@ -1,28 +1,177 @@
+// import './Sidebar.css'
+// import React,{useState , useEffect} from 'react';
+// import img1 from "../Img/hambarger.png";
+// import img2 from "../Img/Youtube logo.png";
+// // import img3 from "../Img/home.png";
+// // import img4 from "../Img/explore.png";
+// // import img5 from "../Img/shorts.png";
+// // import img6 from "../Img/subscription.png";
+// // import img7 from "../Img/library.png";
+// // import img8 from "../Img/history.png";
+// // import img9 from "../Img/your_video.png";
+// // import img10 from "../Img/watch_later.png";
+// // import img11 from "../Img/liked.png";
+// // import img12 from "../Img/down_arrow.png";
+// import img13 from "../Img/Nadir.png";
+// import img14 from "../Img/Coke.png";
+// import img15 from "../Img/MKBHD.png";
+// import img16 from "../Img/Figma.png";
+// import img17 from "../Img/ATC.png";
+// // import { response } from 'express';
+// // import { data } from '../server.js';
+
+
+
+// function Sidebar() {
+
+//     // const data = [
+//     //     {img:img3, title:"Home"},
+//     //     {img:img4, title:"Explores"},
+//     //     {img:img5, title:"Shorts"},
+//     //     {img:img6, title:"subscription"}
+//     // ];
+
+//     // const data2 = [
+//     //     {img:img7, title:"Library"},
+//     //     {img:img8, title:"History"},
+//     //     {img:img9, title:"Your Videos"},
+//     //     {img:img10, title:"Watch Later"},
+//     //     {img:img11, title:"Liked Videos"},
+//     //     {img:img12, title:"Show more"}
+//     // ];
+
+//     const data3 = [
+//         {img:img13, title:"Nadir On The Go"},
+//         {img:img14, title:"Coke Studio Bangla"},
+//         {img:img15, title:"MKBHD"},
+//         {img:img16, title:"Figma"},
+//         {img:img17, title:"Android ToTo C..."},
+//         {img:"https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/02.youtube-clone/assets/Ellipse%201%20(5).png?raw=true", title:"AI ux.com"}
+//     ];
+
+
+
+//     const[photo , setphoto] = useState([]);
+//     useEffect ( ()  => {
+//         fetch('https://api-krmy.onrender.com/data')
+//         .then((response)=> response.json())
+//         .then((data)=>setphoto(data))
+//         .catch((error)=>console.log(error));
+//     }, [1]);
+
+//     const[photo2, setphoto2] = useState([]);
+//     useEffect ( ()  => {
+//         fetch('https://api-krmy.onrender.com/data2')
+//         .then((response)=> response.json())
+//         .then((data2)=>setphoto2(data2))
+//         .catch((error2)=>console.log(error2));
+//     }, []);
+
+
+//     return (
+
+//         <>
+
+//             <div className="container">
+//                 <img src={img1} alt="hamburger menu" />
+//                 <img className="youtube_logo" src={img2} alt="YouTube logo" /><br />
+
+
+//                 {/* <img className="home" src={img3} alt="home icon" />
+//                 <p className="Hunger">Home</p>
+//                 <img src={img4} alt="explore icon" />
+//                 <p className="Hunger">Explores</p>
+//                 <img src={img5} alt="shorts icon" />
+//                 <p className="Hunger">Shorts</p>
+//                 <img src={img6} alt="subscription icon" />
+//                 <p className="Hunger">subscription</p> */}
+
+
+//                 {photo.map((i)=>(
+//                     <>
+//                     <img src={i.img} alt="" />
+//                     <p className="Hunger">{i.title}</p>
+//                     </>
+//                 ))}
+
+
+//                 <hr />
+
+
+//                 {/* <img src={img7} alt="library icon" />
+//                 <p className="Hunger">Library</p>
+//                 <img src={img8} alt="history icon" />
+//                 <p className="Hunger">History</p>
+//                 <img src={img9} alt="your videos icon" />
+//                 <p className="Hunger">Your Videos</p>
+//                 <img src={img10} alt="watch later icon" />
+//                 <p className="Hunger">Watch Later</p>
+//                 <img src={img11} alt="liked videos icon" />
+//                 <p className="Hunger">Liked Videos</p>
+//                 <img src={img12} alt="show more icon" />
+//                 <p className="Hunger">Show more</p> */}
+
+
+//                 {photo2.map((i)=>(
+//                     <>
+//                         <img src={i.img} alt="" />
+//                         <p className="Hunger">{i.title}</p>
+//                     </>
+//                 ))}
+
+
+//                 <hr />
+
+//                 <p className="Subscription">SUBSCRIPTION</p>
+
+//                 {/* <img src={img13} alt="Nadir on the Go icon" />
+//                 <p className="Hunger">Nadir On The Go</p>
+//                 <img src={img14} alt="Coke Studio Bangla icon" />
+//                 <p className="Hunger">Coke Studio Bangla</p>
+//                 <img src={img15} alt="MKBHD icon" />
+//                 <p className="Hunger">MKBHD</p>
+//                 <img src={img16} alt="Figma icon" />
+//                 <p className="Hunger">Figma</p>
+//                 <img src={img17} alt="Android ToTo Channel icon" />
+//                 <p className="Hunger">Android ToTo C...</p>
+//                 <img src='https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/02.youtube-clone/assets/Ellipse%201%20(5).png?raw=true'/>
+//                 <p className="Hunger">AI ux.com</p> */}
+                
+
+//                 {data3.map((i)=>(
+//                     <>
+//                         <img src={i.img} alt="" />
+//                         <p className="Hunger">{i.title}</p>
+//                     </>
+//                 ))}
+
+
+//             </div>
+//         </>
+
+//     )
+
+// }
+
+// export default Sidebar
+
+
+
+
+
 import './Sidebar.css'
 import React,{useState , useEffect} from 'react';
 import img1 from "../Img/hambarger.png";
 import img2 from "../Img/Youtube logo.png";
-// import img3 from "../Img/home.png";
-// import img4 from "../Img/explore.png";
-// import img5 from "../Img/shorts.png";
-// import img6 from "../Img/subscription.png";
-// import img7 from "../Img/library.png";
-// import img8 from "../Img/history.png";
-// import img9 from "../Img/your_video.png";
-// import img10 from "../Img/watch_later.png";
-// import img11 from "../Img/liked.png";
-// import img12 from "../Img/down_arrow.png";
 import img13 from "../Img/Nadir.png";
 import img14 from "../Img/Coke.png";
 import img15 from "../Img/MKBHD.png";
 import img16 from "../Img/Figma.png";
 import img17 from "../Img/ATC.png";
-// import { response } from 'express';
-// import { data } from '../server.js';
 
 
 
-function Sidebar() {
+function Sidebar({getDataFile}) {
 
     // const data = [
     //     {img:img3, title:"Home"},
@@ -50,21 +199,22 @@ function Sidebar() {
     ];
 
 
+
     const[photo , setphoto] = useState([]);
     useEffect ( ()  => {
         fetch('https://api-krmy.onrender.com/data')
         .then((response)=> response.json())
         .then((data)=>setphoto(data))
         .catch((error)=>console.log(error));
-    });
+    }, [1]);
 
     const[photo2, setphoto2] = useState([]);
     useEffect ( ()  => {
         fetch('https://api-krmy.onrender.com/data2')
         .then((response)=> response.json())
         .then((data2)=>setphoto2(data2))
-        .catch((error)=>console.log(error));
-    });
+        .catch((error2)=>console.log(error2));
+    }, [1]);
 
 
     return (
@@ -88,8 +238,10 @@ function Sidebar() {
 
                 {photo.map((i)=>(
                     <>
+                    <div className="new7" onClick={()=>{getDataFile(i.title)}}>
                     <img src={i.img} alt="" />
                     <p className="Hunger">{i.title}</p>
+                    </div>
                     </>
                 ))}
 
@@ -113,8 +265,11 @@ function Sidebar() {
 
                 {photo2.map((i)=>(
                     <>
+                    <div>
                         <img src={i.img} alt="" />
                         <p className="Hunger">{i.title}</p>
+                    </div>
+                        
                     </>
                 ))}
 
@@ -153,3 +308,7 @@ function Sidebar() {
 }
 
 export default Sidebar
+
+
+
+
